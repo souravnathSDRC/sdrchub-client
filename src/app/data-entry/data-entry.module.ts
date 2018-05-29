a6e5e1619f038f2e8e633f41e1a98b69d9fb8101
souravnathSDRC <souravnath@sdrc.co.in>
2018-05-29 14:31:58 +0530

    first commit

---
+++ src/app/data-entry/data-entry.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DataEntryRoutingModule } from './data-entry-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    DataEntryRoutingModule
+  ],
+  declarations: []
+})
+export class DataEntryModule { }
---
+++ src/app/data-view/data-view.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DataViewRoutingModule } from './data-view-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    DataViewRoutingModule
+  ],
+  declarations: []
+})
+export class DataViewModule { }
